d39a90342d046951baf41e8391c64d4fc5102155
chickenriceplatter <chickenriceplatter@gmail.com>
2015-05-12 00:03:08 -0400

    add unicorn

--- Gemfile
+++ Gemfile
@@ -29,10 +29,12 @@ gem 'bootstrap-sass', '~> 3.3.4'
 # gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
-# gem 'unicorn'
+gem 'unicorn'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
+gem 'mina', group: :development
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/unicorn.rb
@@ -0,0 +1,23 @@
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/var/app/dummy"
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/var/app/dummy/tmp/pids/unicorn.pid"
+
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/var/app/dummy/log/unicorn.stderr.log"
+stdout_path "/var/app/dummy/log/unicorn.stdout.log"
+
+# Unicorn socket
+listen "/var/run/unicorn.todo.sock"
+
+# Number of processes
+# worker_processes 4
+worker_processes 2
+
+# Time-out
+timeout 30
