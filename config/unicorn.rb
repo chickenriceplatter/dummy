06750a434ac5e16646594b1a175fed1d6a40cd41
chickenriceplatter <chickenriceplatter@gmail.com>
2015-08-10 02:18:19 -0400

    changed unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 # Set the working application directory
 # working_directory "/path/to/your/app"
-working_directory "/data/dummy/current"
+working_directory "/home/deploy/dummy/current"
 
 # Unicorn PID file location
 # pid "/var/run/engineyard/unicorn_cdc.pid"
@@ -9,15 +9,14 @@ pid "/var/run/deploy/unicorn.pid"
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/data/dummy/shared/log/unicorn.stderr.log"
-stdout_path "/data/dummy/shared/log/unicorn.stdout.log"
+stderr_path "/home/deploy/dummy/shared/log/unicorn.stderr.log"
+stdout_path "/home/deploy/dummy/shared/log/unicorn.stdout.log"
 
 # Unicorn socket
 listen "/var/run/deploy/unicorn.todo.sock"
 
 # Number of processes
-# worker_processes 4
-worker_processes 2
+worker_processes 1
 
 # Time-out
 timeout 30
