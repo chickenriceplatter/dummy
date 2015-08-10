7bf873e60ca1045f89d30b4f065351678ee666e9
chickenriceplatter <chickenriceplatter@gmail.com>
2015-08-10 12:27:25 -0400

    updated unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,8 +9,8 @@ pid "/var/run/deploy/unicorn.pid"
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/home/deploy/dummy/shared/log/unicorn.stderr.log"
-stdout_path "/home/deploy/dummy/shared/log/unicorn.stdout.log"
+stderr_path "/home/deploy/app/dummy/shared/log/unicorn.stderr.log"
+stdout_path "/home/deploy/app/dummy/shared/log/unicorn.stdout.log"
 
 # Unicorn socket
 listen "/var/run/deploy/unicorn.todo.sock"
